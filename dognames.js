e80fe9ab4243a317bd752d503ced8c87b78c239f
nicobell <nico.bellucco@gmail.com>
2020-11-13 16:37:00 +0100

    handlers

--- dognames.js
+++ dognames.js
@@ -23,3 +23,43 @@ exports.handler = async function (event, context) {
     //console.log('RESPONSE :' + JSON.stringify(response));
     return response;
 };
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Welcome to dog names';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+};
+
+const ErrorHandler = {
+    canHandle(handlerInput) {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log('Error handled: ' + JSON.stringify(error.message));
+        // console.log('Original Request was:', JSON.stringify(handlerInput.requestEnvelope.request, null, 2));
+
+        const speechText = 'Sorry, your skill encountered an error';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(false)
+            .getResponse();
+    }
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log('Session ended with reason: ', JSON.stringify(handlerInput.requestEnvelope.request.reason));
+        //any cleanup logic goes here
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
